1faf22f5060774daf7bc950c8e45b12d89d116cd
Daniela Kesselman <d.p.kesselman@gmail.com>
2024-10-14 18:33:14 -0300

    Particulas y shaders

--- Assets/Scripts/AddPoints.cs
+++ Assets/Scripts/AddPoints.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class AddPoints : MonoBehaviour
 {
     public int pointsToAdd;
+    [SerializeField] private ParticleSystem particles;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {
-            PointSytem.Instance.AddingPoints(pointsToAdd);
+            particles.Play();
+            PointSytem.Instance.AddingPoints(pointsToAdd);            
         }
     }
 }
