25ab3abca2f6e9c6e5703edcde1252e1e073e58f
Daniela Kesselman <d.p.kesselman@gmail.com>
2024-10-22 21:15:49 -0300

    EnemyAI

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float chaseSpeed;
+    [SerializeField] private float _distance;
+    [SerializeField] private float stopDistance;
+    [SerializeField] private float returnDistance;
+    private bool flee = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float distance = Vector2.Distance(transform.position, player.position);
+        if (distance > _distance || distance <= stopDistance) return;
+
+        if (!flee)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, chaseSpeed * Time.deltaTime);
+        }
+        else
+        {
+            if (distance > returnDistance) flee = false;
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -1 * chaseSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            flee = true;
+        }
+    }
+}
